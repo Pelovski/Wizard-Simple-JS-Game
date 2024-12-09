e713e554fa74bbf85fea979cbdabdaa9c33d51d7
Pavel Pelovski <git.pelovski@gmail.com>
2024-12-09 23:50:54 +0200

    separate login into diff files

---
+++ src/js/gameObjects.js
@@ -0,0 +1,9 @@
+const startScreen = document.querySelector('.start-screen');
+const gameScreen = document.querySelector('.game-screen');
+
+function initGameObject(){
+    return{
+        startScreen,
+        gameScreen
+    };
+}
